957442d5a8d0018896e4777423c2c6e94ff4ecd0
pm <pm@101media.ru>
2023-11-03 15:33:44 +0300

    HasMentions trait

---
+++ src/Traits/HasMentions.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Codewiser\Notifications\Traits;
+
+use Codewiser\Notifications\Builders\NotificationBuilder;
+use Codewiser\Notifications\Models\DatabaseNotification;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+/**
+ * For Models that are mentioned in Notifications.
+ *
+ */
+trait HasMentions
+{
+    /**
+     * Notifications, where the Model was mentioned.
+     */
+    public function mentions(): HasMany|NotificationBuilder
+    {
+        $type = NotificationBuilder::morph($this);
+
+        return $this->hasMany(DatabaseNotification::class, "data->options->data->bind->$type");
+    }
+}
